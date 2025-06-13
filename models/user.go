32c139da72542f75033a963d14b2eeab1b698518
Nguyễn Trung Nghĩa <59058840+NguyenTrungNghia1996@users.noreply.github.com>
2025-06-13 22:52:08 +0700

    fix password json tag to parse login

--- models/user.go
+++ models/user.go
@@ -5,6 +5,6 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type User struct {
 	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Username string             `bson:"username" json:"username"`
-	Password string             `bson:"password,omitempty" json:"-"`
+	Password string             `bson:"password,omitempty" json:"password,omitempty"`
 	Role     string             `bson:"role" json:"role"`
 }
